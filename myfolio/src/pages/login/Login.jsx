e0df6e85fcc06775a6f10b5517940cf47743fb78
yossogood8611 <rooproop1111@naver.com>
2021-07-31 16:55:43 +0900

    로그인 css 약간

---
+++ myfolio/src/pages/login/Login.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import "./login.css";
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
